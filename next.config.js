614845fef569717a59e37e1528e1b1c665a23dcf
finena68-del <finena68@gmail.com>
2026-02-12 16:36:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: [
-            'media.api-sports.io'
+            'media.api-sports.io',
             'media-1.api-sports.io',
             'media-2.api-sports.io',
             'media-3.api-sports.io'
